af77c2e49d65d708032724219a09477f4c3e8c4c
samikhan4475 <samiwebevis936@gmail.com>
2026-09-09 15:01:28 +0500

    responsive industries section

--- ui/industries/industries.jsx
+++ ui/industries/industries.jsx
@@ -6,19 +6,25 @@ const Industries = () => {
     <Flex gap="4" direction="column" mb='6'>
       <Flex direction="column" gap="1">
         <Text
-          className="text-[#1e2761] text-[37.3333px]"
+          className="text-[#1e2761] xl:text-[37.3333px] md:text-[37.3333px] lg:text-[37.3333px] sm:text-[37.3333px] max-sm:text-[32px]"
           weight="bold"
         >
           Industries I Serve
         </Text>
 
-        <Text className="text-[#6b7280] text-[17.3333px]">
+        <Text className="text-[#6b7280] xl:text-[17.3333px] lg:text-[17.3333px] md:text-[17.3333px] sm:text-[17.3333px] max-sm:text-[15px]">
           Cross-sector recruitment expertise, built from real placements in
           each field.
         </Text>
       </Flex>
 
-      <Grid columns="3" gap="6">
+      <Grid columns={{
+        initial: "1",
+        xs: '2',
+        sm: '2',
+        md: "3",
+        lg: "3",
+      }} gap={{initial:'4',xs:'4',sm:"6"}}>
         {industriesData.map((industry, index) => {
           const isImage = industry.icon.startsWith('http')
 
@@ -60,14 +66,14 @@ const Industries = () => {
       </Grid>
 
 
-      <Flex  gap="1" className='bg-[#1e2761] mt-14 p-5 rounded-xl' justify='between' align='center'>
+      <Flex gap={{initial:'4',xs:'4',sm:"1"}} className='bg-[#1e2761] flex-nowrap max-sm:flex-wrap sm:mt-14 max-sm:mt-8 p-5 rounded-xl' justify='between' align='center'>
         <Text
-          className="text-white text-[16px]"
+          className="text-white xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[15px]"
         >
           Don't see your industry listed? Let's talk about your specific hiring need.
         </Text>
 
-        <Button className='h-12! text-[15.3333px] bg-white! text-[#1e2761]! rounded-lg! w-[20%]!'>Contact Me</Button>
+        <Button className='h-12! text-[15.3333px] bg-white! text-[#1e2761]! rounded-lg! sm:w-[20%]! max-sm:w-full!'>Contact Me</Button>
 
       </Flex>
     </Flex>
